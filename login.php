d220838547464eaec09e4112d652c932d9d561b0
MauriRamirez <mauri.ra99@gmail.com>
2016-06-02 16:47:53 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+	<title>Login Below</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+	<div class="header">
+		<a href="index.php">INTER</a>
+	</div>
+	
+	<h1>Login</h1>
+	
+	<span> or <a href="register.php">register here</a></span>
+	
+	<form action="login.php" method="POST">
+		<input type="text" placeholder="Enter your email" name="email">
+		<input type="password" placeholder="and password" name="password">
+		<input type="submit">
+
+	</form>
+</body>
+</html>
