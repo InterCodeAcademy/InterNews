87e114ed411d7af9e93a5a475100e9415aa1a049
Mario Scappini <makio.scappini@gmail.com>
2016-06-25 16:10:54 -0400

    Visualizacion de noticias

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 include('model/sql.php');
-SQLQuery("SELECT * FROM `usuarios`","username")
+$results = array();
+$results =  SQLQuery($GETNEWSLIST,"ALL");
+$json = 'JsonDescriptions/'.$results['descJson'].'.json';
+$string = file_get_contents($json);
+
+$jsonIterator = new RecursiveIteratorIterator(
+    new RecursiveArrayIterator(json_decode($string, TRUE)),
+    RecursiveIteratorIterator::SELF_FIRST);
+
+foreach ($jsonIterator as $key => $val) {
+    if($key == "img"){
+        
+        echo '<img src="JsonDescriptions/thumb/'.$val.'" height="250" width="250" />';
+        echo $val;
+    }else{
+        echo $val.'<br/>';
+    }
+        
+    
+}
 ?>
--- model/sql.php
+++ model/sql.php
@@ -1,20 +1,32 @@
 <?php 
+$GETUSERS = "SELECT * FROM `usuarios`";
+$GETNEWSLIST = "SELECT * FROM `noticias`";
+
 function SQLQuery($query, $get){ // Llamar SQLQuery("COMANDO SQL", DATOS QUE SE NECESITAN)
-$server = "localhost";
-$usuario = "root";
-$password ="";
-$db = "inter_news";
-$conn =  mysqli_connect($server,$usuario,$password,$db) or die("Error connecting");
-$stmt = mysqli_prepare($conn,$query);
-mysqli_stmt_execute($stmt);
-$result = $stmt->get_result();
-$resultarray = array();
-if(mysqli_num_rows($result)){
-	while ($resultarray = mysqli_fetch_assoc($result)) {
-		echo $resultarray[$get];
-	}
+    $server = "localhost";
+    $usuario = "root";
+    $password ="";
+    $db = "inter_news";
+    $conn =  mysqli_connect($server,$usuario,$password,$db) or die("Error connecting");
+    $stmt = mysqli_prepare($conn,$query);
+        mysqli_stmt_execute($stmt);
+        $result = $stmt->get_result();
+        $resultarray = array();
+    
+    if(mysqli_num_rows($result)){
+	       while ($resultarray = mysqli_fetch_assoc($result)) {
+               if($get == "ALL"){
+                     return $resultarray;
+               }else{
+                    return $resultarray[$get];
+               }
+		      
+	       }
 	
-}mysqli_close($conn);
+    }mysqli_close($conn);
+
 }
+
+
 
 ?>
