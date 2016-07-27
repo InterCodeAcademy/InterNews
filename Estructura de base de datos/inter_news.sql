f13aa6b1cdb9f55b4e8762430b4abfbc1635706c
MarioScappini <makio.scappini@gmail.com>
2016-07-27 18:41:19 -0400

    cambios leves 2

--- Estructura de base de datos/inter_news.sql
+++ Estructura de base de datos/inter_news.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10.12
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.5.199.2:3306
--- Generation Time: May 28, 2016 at 05:01 PM
--- Server version: 5.5.45
--- PHP Version: 5.3.3
+-- Host: localhost
+-- Generation Time: Jul 28, 2016 at 12:40 
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `inter_news`
@@ -23,21 +23,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `noticias`
+--
+
+CREATE TABLE `noticias` (
+  `thumb` varchar(50) NOT NULL,
+  `ID` int(5) NOT NULL,
+  `noticiaJson` varchar(50) NOT NULL,
+  `descJson` varchar(50) NOT NULL,
+  `autor` varchar(50) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `noticias`
+--
+
+INSERT INTO `noticias` (`thumb`, `ID`, `noticiaJson`, `descJson`, `autor`, `fecha`) VALUES
+('abc', 1, 'abcN.json', 'abcD.json', 'Mario Scappini', '2016-06-25 18:35:45'),
+('1234', 2, 'asdf.json', 'asdf.json', 'mario', '2016-06-25 23:57:45'),
+('none', 5, 'LPCRI.json', 'LPCRI.json', 'ICA', '2016-07-25 23:30:56');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `usuarios`
 --
 
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `ID` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuarios` (
+  `ID` int(10) NOT NULL,
   `username` varchar(20) NOT NULL,
   `mail` varchar(60) NOT NULL,
   `ip` varchar(20) NOT NULL,
   `passw` varchar(25) NOT NULL,
   `class` char(1) NOT NULL DEFAULT 'U',
   `name` varchar(20) DEFAULT NULL,
-  `lname` int(30) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `lname` int(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `usuarios`
@@ -46,6 +68,38 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 INSERT INTO `usuarios` (`ID`, `username`, `mail`, `ip`, `passw`, `class`, `name`, `lname`) VALUES
 (1, 'ICA', 'intercode.noreply@gmail.com', '125.0.0', 'Intercode16', 'A', 'ICA', NULL);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `ID` (`ID`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
