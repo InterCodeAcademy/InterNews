4d55cf579ef11f6a66610924dfa260cd157ae80a
MauriRamirez <mauri.ra99@gmail.com>
2016-08-04 16:33:28 -0400

    Update Dashboard.php

--- admin/Dashboard.php
+++ admin/Dashboard.php
@@ -4,6 +4,9 @@ if(isset($_SESSION['USER'])){
                     echo '<a href="modal/logout" style="text-align:;">Log Out</a><br/>';
                     echo '<img src="../model/Perfiles/'.$_SESSION['USER'].'/pp.jpg" height="100" width="100"/> <br/>';
                     echo '<a href="write">Write</a>';
+                    
+
+}
                         
                         
                         
@@ -11,6 +14,14 @@ if(isset($_SESSION['USER'])){
     
     header("Location: http://localhost/InterNews/admin/");
 exit();
-
+     function Titulo($query){ // Llamar SQLQuery("COMANDO SQL", DATOS QUE SE NECESITAN)
+    $server = "localhost";
+    $usuario = "root";
+    $password ="";
+    $db = "inter_news";
+    $conn =  mysqli_connect($server,$usuario,$password,$db) or die("Error connecting");
+    $results =  SQLQuery("SELECT * FROM `noticias`","Titulos");
+     
+    mysqli_close($conn);
 }
 ?>
